17bc9e009c9b1ccae0801abad06a7f97afb41936
sampat211 <56751384+sampat211@users.noreply.github.com>
2021-06-01 10:06:48 +0530

    Add files via upload

---
+++ Part1/1.3/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const course = 'Mearn Stack Development'
+  const part1 = {
+    name: 'MongoDB',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Express.js',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'Angular CLI',
+    exercises: 14
+  }
+  const part4 = {
+    name: 'React.js',
+    exercises: 5
+  }
+  const part5 = {
+    name: 'Node.js',
+    exercises: 7
+  }
+
+  return (
+    <div>
+      <h1>{course}</h1>
+	  <p>{part1.name}: {part1.exercises}</p>
+	  <p>{part2.name}: {part2.exercises}</p>
+	  <p>{part3.name}: {part3.exercises}</p>
+    <p>{part4.name}: {part4.exercises}</p>
+    <p>{part5.name}: {part5.exercises}</p>
+	  <p>Number of exercises: {part1.exercises + part2.exercises + part3.exercises + part4.exercises + part5.exercises}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
