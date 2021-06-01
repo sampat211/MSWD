95af306b9014e1d3f6d9f1ce91006173ffd16557
sampat211 <56751384+sampat211@users.noreply.github.com>
2021-06-01 10:08:14 +0530

    Add files via upload

---
+++ Part1/1.4/src/App.js
@@ -0,0 +1,71 @@
+const App = () => {
+	const course = 'Mearn Stack Development'
+	const parts = [
+		{
+		  name: 'MongoDB',
+		  exercises: 10
+		},
+		{
+            name: 'Express.js',
+            exercises: 7
+        },
+        {
+            name: 'Angular CLI',
+            exercises: 14
+        },
+        {
+            name: 'React.js',
+            exercises: 5
+        },
+        {
+            name: 'Node.js',
+            exercises: 7
+        }
+	]
+
+	return (
+		<div>
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total parts={parts} />
+		</div>
+	)
+};
+
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.course}</h1>
+		</div>
+	)
+};
+
+const Content = (props) => {
+	const lists = props.parts.map(function(item) {
+		return (
+			<div>
+				<p> {item.name}: {item.exercises}</p>
+			</div>
+		)
+	})
+	
+	return lists
+};
+
+const Total = (props) => {
+	var score = 0
+	
+	const lists = props.parts.map(function(item) {
+		
+		score = score + item.exercises 
+		
+	})
+	
+	return (
+		<div>
+			<p>Number of exercises: {score} </p>
+		</div>
+	)
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
