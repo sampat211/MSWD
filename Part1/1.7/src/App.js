e8cff488a678145bde1b9fc4ca5bc36e0588ff79
sampat211 <56751384+sampat211@users.noreply.github.com>
2021-06-01 17:32:18 +0530

    Update App.js

--- Part1/1.7/src/App.js
+++ Part1/1.7/src/App.js
@@ -8,17 +8,17 @@ const App = () => {
 	
 	return (
 		<div>
-			<h1>Please Give Feedback, Thank you!</h1><br/>
+			<h1>Please Give Feedback this will be impotant to us, Thank you!</h1><br/>
 			<button onClick = {() => setGood(good + 1)}>Good</button>
 			<button onClick = {() => setNeutral(neutral + 1)}>Neutral</button>
 			<button onClick = {() => setBad(bad + 1)}>Bad</button>
 			<br/>
 			<h1>Statistics</h1>
 			<br/>
-			<h3>Good: {good}</h3>
-			<h3>Neutral: {neutral}</h3>
-			<h3>Bad: {bad}</h3>
-			<h3>All: {good + neutral + bad}</h3>
+			<h3>Good Feedback: {good}</h3>
+			<h3>Neutral Feedback: {neutral}</h3>
+			<h3>Bad Feedback: {bad}</h3>
+			<h3>All Feedbacks: {good + neutral + bad}</h3>
 			<h3>Average: {(good - bad)/(good + neutral + bad)}</h3>
 			<h3>Positive: {(good + neutral)/(good + neutral + bad)}</h3>
 		</div>
