0cf8770f105784d1d9e736202c7c90d3b440ad56
sampat211 <56751384+sampat211@users.noreply.github.com>
2021-06-04 18:37:30 +0530

    Update index.js

--- Part2/2.16/src/index.js
+++ Part2/2.16/src/index.js
@@ -1,1 +1,13 @@
-hi
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import axios from 'axios'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
